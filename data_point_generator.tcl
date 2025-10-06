87a260de179803681bf0080f37a473dd755c4646
David Gae <ddhgae@gmail.com>
2025-10-06 10:10:21 -0700

    Add files via upload

---
+++ data_point_generator.tcl
@@ -0,0 +1,47 @@
+		top right point
+		#corner 1 
+		$X1 > $X2 
+		$Y1 > $Y2 	
+		$Z1 > $Z2 
+		#corner 2 
+		-$X1 < $X2 		
+		-$Y1 < $Y2 	
+		-$Z1 < $Z2 		
+		#corner 3 
+		-$X1 < $X2		
+		-$Y1 < $Y2
+		 $Z1 > $Z2 
+		#corner 5 
+		 $X1 > $X2 		
+		-$Y1 < $Y2 		
+		 $Z1 > $Z2 		
+                #corner 7 
+		-$X1 < $X2 
+		$Y1 > $Y2 
+		$Z1 > $Z2 
+		#corner 8 
+		$X1 > $X2 
+		$Y1 > $Y2 		
+		-$Z1 < $Z2 
+
+		corner 2 point reassign 
+		#corner 6
+		$X1 > $X2
+		-$Y1 < $Y2		
+                $Z1 > $Z2
+		#corner 4
+		-$X1< $X2
+		-$Y1< $Y2
+		$Z1 > $Z2 	
+		#corner 2
+		-$X1 < $X2 		
+	        -$Y1 < $Y2 		
+		 $Z1 > $Z2
+		#corner 7
+		$X1 > $X2
+		$Y1 > $Y2
+		$Z1 < $Z2
+
+
+Smaller box within grid. 
+faster box generator.  
